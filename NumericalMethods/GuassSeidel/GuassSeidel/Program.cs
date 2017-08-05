dcb3e19ae718dc930aaf8abb7d17277374971caa
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2017-08-05 11:01:02 +0100

    Update Program.cs

--- NumericalMethods/GuassSeidel/GuassSeidel/Program.cs
+++ NumericalMethods/GuassSeidel/GuassSeidel/Program.cs
@@ -7,22 +7,22 @@ namespace GuassSeidel
         public static void Main()
         {
 
-			Double[,] linearEquation = new Double[,]{
-				 { 25, 2, 1, 4 },
-				 { 81, -4, -2, 2 },
-				 { 144, 3, 5, 1 }
-			};
+			Double[,] linearEquation = new Double[,]{ 
+			{ 25, 5, 1, 106.8 },
+			{ 64, 8, 1, 177.2 },
+			{ 144, 12, 1, 292.2 } }
+			;
 
 
 			Double[][] matrix = new Double[][]{
-                new Double[] { 25, 2, 1 },
-                new Double[]  { 81, -4, -2 },
-                new Double[] { 144, 3, 5 }
+                new Double[] { 25, 5, 1, 106.8 },
+                new Double[]  { 64, 8, 1, 177.2 },
+                new Double[] { 144, 12, 1, 292.2 }
             };
 
             DisplayMatrix(linearEquation);
 
-            Double[] bee = { 4, 2, -1 };
+            Double[] bee = { 106.8, 177.2, 292.2 };
             Double[] guess = { 1, 1, 1 };
 
 			Console.WriteLine("Enter the initial guess"); 
