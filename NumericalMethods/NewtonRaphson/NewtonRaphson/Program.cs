efa3a8e1aa0542abb2416f8a551f24134a64e55f
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2017-08-05 11:07:00 +0100

    Update Program.cs

--- NumericalMethods/NewtonRaphson/NewtonRaphson/Program.cs
+++ NumericalMethods/NewtonRaphson/NewtonRaphson/Program.cs
@@ -6,10 +6,7 @@ namespace NewtonRaphson
     {
         public static void Main(string[] args)
         {
-			// x^3 + 3x^2 - 3x - 4 = 0
-
-			//Double[] equation = { 1, 3, -3, -4 };
-			Double[] equation = { 1, 2, 1 };
+	    Double[] equation = { 5, 6, 1 };
             Console.Write("Enter initial guess: ");
             Double guess = Convert.ToDouble(Console.ReadLine());
             NewtonRaphson newtonRaphson = new NewtonRaphson(equation, guess);
